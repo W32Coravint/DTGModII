8270e4ef9423d2cac912205b691e3edfe5d3d388
W32Coravint <w32coravint@gmail.com>
2016-04-17 06:45:05 +0800

    Started work on Deseret Clockbow

---
+++ src/main/java/tutorial/coravint/Items/DsrtBow.java
@@ -0,0 +1,12 @@
+package tutorial.coravint.Items;
+
+import net.minecraft.item.ItemBow;
+
+public class DsrtBow extends ItemBow {
+	public DsrtBow(){
+		super();
+		setMaxStackSize(64);
+		setUnlocalizedName("shotgun");
+		setTextureName("dtgmodii:dsrtbow");
+	}
+}
